d6c49155fff90708521f460023332112932b3ef3
Cesar Romero <rasec.romero46@gmail.com>
2025-06-19 10:17:44 -0600

    feat: update api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https/localhost:8000", // poner la ip despues del servidor de digital en el archivo .env
+  baseURL: "http://localhost:8000",
 });
 
 export default api;
